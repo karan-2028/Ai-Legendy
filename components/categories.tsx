ae9bea82747772536dbc06b606437975bd0946b3
karan-2028 <karankumar25642@gmail.com>
2024-03-01 20:26:14 +0530

    created category in database

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,15 +1,59 @@
-"use client";
+"use strict";
 
+import { cn } from "@/lib/utils";
 import {Category} from "@prisma/client";
 
-interface categoriesProps {
+interface CategoriesProps {
     data: Category[];
 };
 
-export const Categories = () => {
+export const Categories = ({
+    data
+}: CategoriesProps) => {
     return (
-        <div>
-            Categories!
+        <div className="w-full overflow-x-auto space-x-2 flex p-1"> 
+            <button className={cn(`
+            flex
+            items-center
+            text-center
+            text-xs
+            md:text-sm
+            px-2
+            md:px-4
+            py-2
+            md:py-3
+            rounded-md
+            bg-primary/10
+            hover:opacity-75
+            transition
+            `)}
+            >
+            Newest
+            </button>
+           {data.map((item)=>(
+           
+           <button 
+           key ={item.id}
+           className={cn(`
+            flex
+            items-center
+            text-center
+            text-xs
+            md:text-sm
+            px-2
+            md:px-4
+            py-2
+            md:py-3
+            rounded-md
+            bg-primary/10
+            hover:opacity-75
+            transition
+            `)}
+            >
+            {item.name}
+            </button>
+           
+           ))}
         </div>
     )
 }
